2c555f221b038622d86ff64fdeb66195a978c1b6
Andy Botting <andy@andybotting.com>
2018-04-03 20:29:57 +1000

    More work

--- lib/drmhelper/config.py
+++ lib/drmhelper/config.py
@@ -2,19 +2,20 @@
 
 REPO_BASE = 'https://github.com/aussieaddons/repo-binary/raw/master/'
 
-WIDEVINECDM_URL = {
-    'Linux-x86_64': 'https://dl.google.com/widevine-cdm/903-linux-x64.zip',
-    'Linux-arm': 'http://odroidxu.leeharris.me.uk/xu3/chromium-widevine-1.4.8.823-2-armv7h.pkg.tar.xz',
-    'Linux-aarch64': 'http://odroidxu.leeharris.me.uk/xu3/chromium-widevine-1.4.8.823-2-armv7h.pkg.tar.xz',
-    'Windows-x86_64': 'https://dl.google.com/widevine-cdm/903-win-x64.zip',
-    'Windows-x86': 'https://dl.google.com/widevine-cdm/903-win-ia32.zip',
-    'Darwin-x86_64': 'https://dl.google.com/widevine-cdm/903-mac-x64.zip'}
+WIDEVINE_CDM_URL = {
+    ('Linux', 'x86_64'): 'https://dl.google.com/widevine-cdm/903-linux-x64.zip',
+    ('Linux', 'arm'): 'http://odroidxu.leeharris.me.uk/xu3/chromium-widevine-1.4.8.823-2-armv7h.pkg.tar.xz',
+    ('Linux', 'aarch64'): 'http://odroidxu.leeharris.me.uk/xu3/chromium-widevine-1.4.8.823-2-armv7h.pkg.tar.xz',
+    ('Windows', 'x86_64'): 'https://dl.google.com/widevine-cdm/903-win-x64.zip',
+    ('Windows', 'x86'): 'https://dl.google.com/widevine-cdm/903-win-ia32.zip',
+    ('Darwin', 'x86_64'): 'https://dl.google.com/widevine-cdm/903-mac-x64.zip'
+}
 
 UNARCHIVE_COMMAND = {
     'Linux-x86_64': '(cd {1} && unzip {0} {2} -d {1} && chmod 755 {1}/{2} && rm -f {0})',
     'Linux-arm': '(cd {1} && tar xJfO {0} usr/lib/chromium/libwidevinecdm.so >{1}/{2} && chmod 755 {1}/{2} && rm -f {0})',
     'Linux-aarch64': '(cd {1} && tar xJfO {0} usr/lib/chromium/libwidevinecdm.so >{1}/{2} && chmod 755 {1}/{2} && rm -f {0})',
-    'Darwin-x86_64': '(cd {1} && unzip {0} {2} -d {1} && chmod 755 {1}/{2} && rm -f {0})'
+    'Darwin-x86_64': '(cd {1} && unzip {0} {2} -d {1} && chmod 755 {1}/{2} && rm -f {0})',
 }
 
 SSD_WV_DICT = {
@@ -24,7 +25,7 @@ SSD_WV_DICT = {
     'Darwin': 'libssd_wv.dylib'
 }
 
-WIDEVINECDM_DICT = {
+WIDEVINE_CDM_DICT = {
     'Android': None,
     'Windows': 'widevinecdm.dll',
     'Linux': 'libwidevinecdm.so',
@@ -45,7 +46,7 @@ ARCH_DICT = {
     'i686': 'x86'
 }
 
-SUPPORTED_PLATFORMS = [
+SUPPORTED_WV_DRM_PLATFORMS = [
     ('Windows', 'x86_64'),
     ('Windows', 'x86'),
     ('Darwin', 'x86_64'),
@@ -66,18 +67,23 @@ WINDOWS_BITNESS = {
 
 
 KODI_NAME = {
-    '17': 'Krypton',
-    '18': 'Leia'
+    12: 'Frodo',
+    13: 'Gotham',
+    14: 'Helix',
+    15: 'Isengard',
+    16: 'Jarvis',
+    17: 'Krypton',
+    18: 'Leia'
 }
 
 MIN_IA_VERSION = {
-    'Krypton': '2.0.7',
-    'Leia': '2.0.10'
+    17: '2.0.7',
+    18: '2.0.10'
 }
 
 CURRENT_IA_VERSION = {
-    'Krypton': {'ver': '2.0.19', 'commit': '9af2121'},
-    'Leia': {'ver': '2.0.10', 'commit': '0c7e975'}
+    17: {'ver': '2.0.19', 'commit': '9af2121'},
+    18: {'ver': '2.0.10', 'commit': '0c7e975'}
 }
 
 MIN_LEIA_BUILD = ('20170818', 'e6b0c83')
--- lib/drmhelper/tests/test_drmhelper.py
+++ lib/drmhelper/tests/test_drmhelper.py
@@ -1,24 +1,27 @@
-from drmhelper import drmhelper
+from drmhelper import helper
+
+import fakes
 
 import mock
 
 import testtools
 
 
+def get_xbmc_cond_visibility(cond):
+    global HACK_PLATFORMS
+    if cond in HACK_PLATFORMS:
+        return True
+
+
 class DRMHelperTests(testtools.TestCase):
 
-    def test_get_info_label(self):
-        dh = drmhelper.DRMHelper()
-        with mock.patch('xbmc.getInfoLabel', return_value='foo'):
-            dh._get_info_label('test_label')
-            self.assertEqual(dh._get_info_label('test_label'), 'foo')
-
-    def test_get_info_label_busy(self):
-        dh = drmhelper.DRMHelper()
-        with mock.patch('xbmc.getInfoLabel', return_value='Busy'):
-            dh._get_info_label('test_label')
-            self.assertEqual(dh._get_info_label('test_label'), None)
-
-    def test_get_system(self):
-        # dh = drmhelper.DRMHelper()
-        pass
+    @mock.patch('xbmc.getCondVisibility')
+    def test_get_system(self, mock_cond_vis):
+        for system in fakes.SYSTEMS:
+            with mock.patch('platform.system', return_value=system['system']):
+                global HACK_PLATFORMS
+                HACK_PLATFORMS = system['platforms']
+                mock_cond_vis.side_effect = get_xbmc_cond_visibility
+                h = helper.DRMHelper()
+                sys = h._get_system()
+                self.assertEqual(sys, system['expected_system'])
